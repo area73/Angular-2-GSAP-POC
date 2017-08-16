16f33d64196706e83cd3a6367859d41119d93f85
legndery <ar_itra@yahoo.com>
2017-08-16 08:59:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { TweenMax } from 'gsap';
+// import * as ScrollMagic from 'ScrollMagic';
+declare var ScrollMagic:any;
+declare var TweenMax:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(){
+    var controller = new ScrollMagic.Controller();
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement: "#trigger1"
+    })
+    .setTween("#animate1", 0.5, {backgroundColor: "green", scale: 2.5}) // trigger a TweenMax.to tween
+    .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+    .addTo(controller);
+  }
 }
